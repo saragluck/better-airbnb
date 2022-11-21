0234ac89f08d86355699ada91937ab6bfa1800b0
Connor La Rocca <connorlarocca@hotmail.com>
2022-11-21 15:11:12 -0500

    model associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  belongs_to :user
+  has_many :room_images
+  has_many :reservations
+  has_many :comments, through: :reviews
+  has_many :ratings, through: :reviews
 end

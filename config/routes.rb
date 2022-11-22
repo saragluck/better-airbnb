f610af0b5169bd3bf2d131f1f8731b02e2c6b2a7
Sara Gluck <srfgglck@gmail.com>
2022-11-22 13:42:41 -0600

    Made some changes idk what

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get "/login" => "sessions#new"
-  get "/logout" => "sessions#destroy"
-
-  resources :reviews
-  resources :reservations
-  resources :users
-  resources :sessions
-  resources :rooms
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  #hello world
-=======
   resources :users
   resources :sessions
   resources :rooms
   resources :reservations
   resources :reviews
   resources :sessions
->>>>>>> 24d05a45b64b646cc7793372ce3b99e42a7562b7
 end
-
-
